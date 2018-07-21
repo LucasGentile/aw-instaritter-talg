2e563b48364535fc2e6766e76d084c8e29e20332
Gentile <Lucas_Gentile@AMER.DELL.COM>
2018-07-21 10:27:03 -0300

    Initial commit

---
+++ src/main/java/br/edu/tasima/aw/talg/awinstarittertalg/AwInstaritterTalgApplication.java
@@ -0,0 +1,12 @@
+package br.edu.tasima.aw.talg.awinstarittertalg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwInstaritterTalgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwInstaritterTalgApplication.class, args);
+	}
+}
